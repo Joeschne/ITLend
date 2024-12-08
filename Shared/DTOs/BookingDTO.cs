a47e99e3b9da394b936050480c8ffe7a1a5740db
Joel Schneider <Joelcschneider@gmx.ch>
2024-12-08 16:54:47 +0100

    Add project structure

---
+++ Shared/DTOs/BookingDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTOs;
+
+public class BookingDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Student Username is required.")]
+    [StringLength(50, ErrorMessage = "Student Username cannot exceed 50 characters.")]
+    public string StudentUsername { get; set; }
+
+    [Required(ErrorMessage = "Laptop Id is required.")]
+    public int LaptopId { get; set; }
+
+    [StringLength(50, ErrorMessage = "Teacher Username cannot exceed 50 characters.")]
+    public string? TeacherUsername { get; set; } // Nullable for optional teacher assignments.
+
+    [Required(ErrorMessage = "Returned status is required.")]
+    public bool Returned { get; set; }
+
+    [Required(ErrorMessage = "Booking DateTime is required.")]
+    public DateTime BookingDateTime { get; set; }
+
+    [Required(ErrorMessage = "Planned Return DateTime is required.")]
+    public DateTime PlannedReturn { get; set; }
+}
+
---
+++ Shared/DTOs/LaptopDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTOs;
+
+public class LaptopDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Availability Status is required.")]
+    public bool AvailabilityStatus { get; set; }
+
+    [Required(ErrorMessage = "Model is required.")]
+    [StringLength(100, ErrorMessage = "Model cannot exceed 100 characters.")]
+    public string Model { get; set; }
+
+    [Required(ErrorMessage = "Serial Number is required.")]
+    [StringLength(50, ErrorMessage = "Serial Number cannot exceed 50 characters.")]
+    public string SerialNumber { get; set; }
+
+    [StringLength(1000, ErrorMessage = "Damage Description cannot exceed 250 characters.")]
+    public string? DamageDescription { get; set; }
+}
+
